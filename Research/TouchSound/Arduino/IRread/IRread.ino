20a0a07280e14d53033591e0614f9d2e10298143
Tmokuzu <126632251+Tmokuzu@users.noreply.github.com>
2024-06-03 04:20:47 +0900

    add Ard

---
+++ Research/TouchSound/Arduino/IRread/IRread.ino
@@ -0,0 +1,31 @@
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println(String(IRread(0) * CheckState(1),0));
+  delay(1);
+}
+
+double IRread(int PinNo) {
+ double ans ;
+ int i ;
+ ans = 0 ;
+ for (i=0 ; i < 1000 ; i++) {
+   ans  = ans + analogRead(PinNo) ;   // 指定のアナログピン(0番)から読取る
+  }
+  return 19501.14 * pow(ans/1000 , -1.256676) * 10; //センサーの値を1000回読み取った平均値を返す(平滑化)
+}
+
+int CheckState(int PinNo) {
+  int i;
+  int ans = 0;
+  int State;
+  for (i=0; i<10 ; i++) {
+    ans = ans + analogRead(PinNo);
+  }
+  if(ans>1200) State=1;
+  else State = 0;
+  return State;
+}
